452d1a9faf7d1c859d9ad610cdd73a89014eeff9
unknown <sbicalho86@gmail.com>
2019-08-10 11:16:31 -0300

    fix UDPServer

--- kurchap2/UDPServer.py
+++ kurchap2/UDPServer.py
@@ -2,12 +2,12 @@ from socket import *
 
 serverPort = 12000
 serverSocket = socket(AF_INET, SOCK_DGRAM)
-serverSocket.bind((’’, serverPort))
+serverSocket.bind(('', serverPort))
 
-print ”The server is ready to receive”
+print ("The server is ready to receive")
 
 while 1:
-    
+
     message, clientAddress = serverSocket.recvfrom(2048)
 
     modifiedMessage = message.upper()
