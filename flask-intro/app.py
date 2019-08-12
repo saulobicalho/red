8a49bdd6555c3d80ea97c74d2d4b972c0898cd2e
unknown <sbicalho86@gmail.com>
2019-08-11 23:03:35 -0300

    introduces index

--- flask-intro/app.py
+++ flask-intro/app.py
@@ -1,13 +1,16 @@
 # import the Flask class from the flask module
-from flask import Flask, render_template, redirect, url_for, request
+from flask import Flask, render_template, redirect, url_for, request, session, flash
 
 # create the application object
 app = Flask(__name__)
 
+app.secret_key = "My precious"
+
 # use decorators to link the function to a url
 @app.route('/')
 def home():
-    return "Hello, World!"  # return a string
+    # return "Hello, World!"  # return a string
+    return render_template('index.html')  # render a template
 
 @app.route('/welcome')
 def welcome():
@@ -18,11 +21,20 @@ def welcome():
 def login():
     error = None
     if request.method == 'POST':
+
         if request.form['username'] != 'admin' or request.form['password'] != 'admin':
             error = 'Invalid Credentials. Please try again.'
         else:
+            session['logged_in']= True
+            flash('You are just logged in!')
             return redirect(url_for('home'))
     return render_template('login.html', error=error)
+
+@app.route('/logout')
+def logout():
+    session.pop('logged_in',None)
+    flash('You are just logged out!')
+    return redirect(url_for('welcome'))
 
 # start the server with the 'run()' method
 if __name__ == '__main__':
